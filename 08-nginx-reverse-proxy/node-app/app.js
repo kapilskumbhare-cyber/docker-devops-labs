741713e3387396105ad090ff0c02728022248988
Kapil Kumbhare <kapilskumbhare@gmail.com>
2026-07-22 14:33:09 +0000

    Remove node_modules from repository

---
+++ 08-nginx-reverse-proxy/node-app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Node app running");
+});
